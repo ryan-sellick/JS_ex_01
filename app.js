74bf8494ed6d62619f0c1c710a1fc359e5d64354
Ryan Sellick <ryan.sellick@live.com>
2020-03-21 17:11:54 -0400

    Add functions: animatedForm, validateUser, error

---
+++ app.js
@@ -0,0 +1,28 @@
+function animatedForm(){
+  const ARROWS = document.querySelectorAll('.fa-arrow-down')
+
+  ARROWS.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+        const INPUT = arrow.previousElementSibling;
+        const PARENT = arrow.parentElement;
+        const NEXTFORM = PARENT.nextElementSibling;
+    });
+  });
+}
+
+function validateUser(user){
+  if (user.value.length < 6){
+    console.log('not enough characters');
+    error('rgb(189, 87, 87)');
+  } else {
+    error('rgb(87, 189, 130)');
+    return true;
+  }
+}
+
+function error (color){
+  document.body.style.backgroundColor = color;
+}
+
+
+animatedForm();
